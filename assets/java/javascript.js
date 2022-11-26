9526a2ffffd8688abfd567f7d6c8b37e098ee644
mijan <mijannurbd@gmail.com>
2022-11-26 12:55:21 +0600

    full site update

--- assets/java/javascript.js
+++ assets/java/javascript.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector('.first-section')
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+const humberger = document.querySelector('.humberger')
+
+humberger.addEventListener('click', () => {
+    const menu = document.querySelector('.main-menu')
+    menu.classList.toggle('active')
+    humberger.classList.toggle('active')
+})
